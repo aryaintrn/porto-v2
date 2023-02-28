dcb9823b55cb1facbe0a58183c2db51622cc9dae
INTRN <68192415+aryaintrn@users.noreply.github.com>
2023-02-28 21:27:53 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@ const header= document.querySelector('header');
 window.addEventListener('scroll', function(){
     header.classList.toggle('sticky', window.scrollY > 100);
 });
+
+Let menu = document.querySelector('#menu-icon');
+Let nav = document.querySelector('.nav');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    nav.classList.toggle('open');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    nav.classList.remove('open');
+}
